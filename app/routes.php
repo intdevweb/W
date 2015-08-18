8a7fb1181a5875a076fe5aaec6bf00c31bb78c1b
intdevweb <jp_informatique@hotmail.fr>
2015-08-18 12:55:16 +0200

    add page about

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,9 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
+		['GET|POST', '/contactez-nous/', 'Default#contact', 'contact'], //ajout de POST 
+		//et de l'adresse par défaut de contctez nous (page contact)
+		//le ddernier parametre est une variable (lien) appelé dans de futurs appels de page 
+		//afin d'eviter d'avoir a ecrire le chemin d'acces en entier
+		['GET|POST', '/About/', 'Default#about', 'about']
 	);
---
+++ app/templates/default/contact.php
@@ -0,0 +1,6 @@
+<?php $this->layout('layout', ['title' => 'A propos']) ?>
+
+<?php $this->start('main_content') ?>
+	<h2>A propos</h2>
+	
+<?php $this->stop('main_content') ?>
